116ceff8f9887a9c395ae1a837498bd1fa9c3cd4
unknown <ashuisalive@gmail.com>
2021-06-21 11:19:45 +0530

    added solutions

--- equilibriumElement.js
+++ equilibriumElement.js
@@ -1,8 +1,27 @@
 function runProgram(input) {
-	// Write code here
+	input = input.trim().split("\n");
+	let size = +input.shift();
+	let arr = input[0].trim().split(" ").map(Number);
+	for (let i = 1; i < size - 1; i++){
+		leftSum = traversalSum(0, i - 1, arr);
+		rightSum = traversalSum(i + 1, size - 1, arr);
+		if (leftSum == rightSum) {
+			console.log(i + 1);
+			return;
+		}
+	}
+	console.log(-1);
+}
+const traversalSum = (start, end, arr) => {
+	let sum = 0;
+	for (let i = start; i <= end; i++){
+		sum += arr[i];
+	}
+	return sum;
 }
 if (process.env.USERNAME === "ASHUTOSH") {
-	runProgram(`Test case`);
+	runProgram(`5
+	3 3 5 5 1`);
 } else {
 	process.stdin.resume();
 	process.stdin.setEncoding("ascii");
